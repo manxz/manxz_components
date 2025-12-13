e081d5141ea6c9dca7de212f94eba9eb2018c097
Dan Ricart <dannj1982@gmail.com>
2025-12-12 19:44:08 -0500

    adding font families

--- src/components/Input/InputGroup.tsx
+++ src/components/Input/InputGroup.tsx
@@ -8,6 +8,7 @@
 import React, { memo, Children, cloneElement, isValidElement } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { COLORS } from '../../styles/colors';
+import { FONT_FAMILIES, FONT_SIZES } from '../../styles/typography';
 
 // ============================================================================
 // TYPES
@@ -147,7 +148,7 @@ const styles = StyleSheet.create({
   errors: {
     color: COLORS.negative,
     fontFamily: FONT_FAMILIES.nunito.medium,
-    fontSize: 16,
+    fontSize: FONT_SIZES.base,
   }
 });
 
